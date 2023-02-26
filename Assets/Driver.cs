7f698da7571543ea3d5d44a0aa405d3f6bc5f560
unAlike <Ryan.Ungerleider@gmail.com>
2023-02-26 14:17:18 -0600

    Individual Pixels working
    Resolution way higher

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -15,6 +15,7 @@ public class Driver : MonoBehaviour {
     int NUM_FDTD = 100;
     Image[] imgs;
     int counter;
+    public float resolution;
     public float timeStep, C, u, e, Mh, Me, time, TProp, dist, add;
 
     float E3, E2, E1, H3, H2, H1 = 0;
@@ -74,12 +75,16 @@ public class Driver : MonoBehaviour {
 
         GameObject grid = GameObject.Find("GridCanvas");
         RectTransform ImgRect = GameObject.Find("SimWindow").GetComponent<RectTransform>();
-        render = new RenderTexture((int)ImgRect.rect.width,(int)ImgRect.rect.height,255);
+        render = new RenderTexture(Width,Height,1);
         render.enableRandomWrite = true;
+        render.filterMode = FilterMode.Point;
         render.Create();
         GameObject.Find("SimWindow").GetComponent<RawImage>().texture = render;
     }
 
+    void Update(){
+        debug();
+    }
     void FixedUpdate(){
         add/=1.5f;
         if(shouldUpdate){
@@ -90,18 +95,17 @@ public class Driver : MonoBehaviour {
             //Update E From H
             //updateEField();
             //EFields[0].Position.y = add;
-            debug();
         }
-        if (counter>10){
+        if (true){
             //randomizeColors();
             counter=0;
             ComputeBuffer col = new ComputeBuffer(HFields.Length,sizeof(float)*9);
             col.SetData(HFields);
             SimCompute.SetBuffer(0, "cells", col);
             SimCompute.SetTexture(0,"Result",render);
-            SimCompute.SetFloat("resolution", 25);
+            SimCompute.SetFloat("resolution", resolution);
             SimCompute.SetFloat("time", counter);
-            SimCompute.Dispatch(0,render.width/10,render.height,1);
+            SimCompute.Dispatch(0,render.width,render.height,1);
 
             col.Release();
 
@@ -110,30 +114,30 @@ public class Driver : MonoBehaviour {
     }
 
 
-    void updateHField(){
-        for(int i=0; i<NUM_FDTD-1;i++){
-            // Update Equation for Every HField
-            HFields[i].Position.x = HFields[i].Position.x + (HFields[i].coef * ((EFields[i+1].Position.y - EFields[i].Position.y))/dist);
+    // void updateHField(){
+    //     for(int i=0; i<NUM_FDTD-1;i++){
+    //         // Update Equation for Every HField
+    //         HFields[i].Position.x = HFields[i].Position.x + (HFields[i].coef * ((EFields[i+1].Position.y - EFields[i].Position.y))/dist);
 
-            Vector3 pos = Hline.GetPosition(i);
-            pos.y = HFields[i].Position.x;
-            Hline.SetPosition(i,pos);
-        }
-        HFields[NUM_FDTD-1].Position.x += (HFields[NUM_FDTD-1].coef * (E3 - EFields[NUM_FDTD-1].Position.y));
-        H3=H2; H2=H1; H1=HFields[0].Position.x;
+    //         Vector3 pos = Hline.GetPosition(i);
+    //         pos.y = HFields[i].Position.x;
+    //         Hline.SetPosition(i,pos);
+    //     }
+    //     HFields[NUM_FDTD-1].Position.x += (HFields[NUM_FDTD-1].coef * (E3 - EFields[NUM_FDTD-1].Position.y));
+    //     H3=H2; H2=H1; H1=HFields[0].Position.x;
 
-    }
-    void updateEField(){
-        EFields[0].Position.y += EFields[0].coef * (HFields[0].Position.y - E3);
-        for(int i=1; i<NUM_FDTD-1; i++){   
-            //Update Equation for Every EField
-            EFields[i].Position.y = EFields[i].Position.y + (EFields[i].coef * (HFields[i].Position.x - HFields[i-1].Position.x)/dist);
-            Vector3 pos = Eline.GetPosition(i);
-            pos.y = EFields[i].Position.y;
-            Eline.SetPosition(i,pos);
-        }
-        E3=E2;E2=E1;E1=EFields[NUM_FDTD-1].Position.y;
-    }
+    // }
+    // void updateEField(){
+    //     EFields[0].Position.y += EFields[0].coef * (HFields[0].Position.y - E3);
+    //     for(int i=1; i<NUM_FDTD-1; i++){   
+    //         //Update Equation for Every EField
+    //         EFields[i].Position.y = EFields[i].Position.y + (EFields[i].coef * (HFields[i].Position.x - HFields[i-1].Position.x)/dist);
+    //         Vector3 pos = Eline.GetPosition(i);
+    //         pos.y = EFields[i].Position.y;
+    //         Eline.SetPosition(i,pos);
+    //     }
+    //     E3=E2;E2=E1;E1=EFields[NUM_FDTD-1].Position.y;
+    // }
 
 
     public void AddDevice(){
@@ -162,11 +166,36 @@ public class Driver : MonoBehaviour {
         if(Input.GetKey(KeyCode.Q)){
             changeColor();
         }
-        else{
-            
+        if(Input.GetKeyDown(KeyCode.W)){
+            for(int i=0; i<HFields.Length;i++){
+                if(HFields[i].Color != Color.green){ 
+                    HFields[i].Color = Color.green;
+                    break;
+                }
+            }
         }
-        if(Input.GetKeyDown(KeyCode.P)){
 
+        if(Input.GetKeyDown(KeyCode.P)){
+            print("Num FDTD: " + NUM_FDTD);
+            print("Width: "+Width + "   Height: " + Height);
+        }
+        if(Input.GetKeyDown(KeyCode.B)){
+            for(int i =0; i<HFields.Length;i++){
+                HFields[i].Color = Color.black;
+            }
         }
     }
+
+    public void UI(){
+        Vector2 pos;
+        RectTransform r = GameObject.Find("SimWindow").GetComponent<RectTransform>();
+        pos = Input.mousePosition;
+        pos.x = Mathf.FloorToInt(pos.x*2/resolution);
+        pos.y = Mathf.FloorToInt(pos.y*2/resolution);
+        print(pos);
+        print(Mathf.FloorToInt(pos.x)+Mathf.FloorToInt(pos.y)*resolution);
+        print(HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Color);
+        HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Color = Color.red;
+    }
+
 }
