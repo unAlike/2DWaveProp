894f33c1d53ff22b91ec79355ba52d92ed79625d
unAlike <Ryan.Ungerleider@gmail.com>
2023-02-26 19:30:03 -0600

    Seperated Fields and even spaced their positions in space

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -10,7 +10,7 @@ public class Driver : MonoBehaviour {
     public ComputeShader SimCompute, CellsCompute;
     public RenderTexture render;
 
-    public int Width, Height = 10;
+    int Width, Height = 10;
 
     int NUM_FDTD = 100;
     Image[] imgs;
@@ -21,10 +21,10 @@ public class Driver : MonoBehaviour {
     float E3, E2, E1, H3, H2, H1 = 0;
     public bool shouldUpdate = true;
     void Start() {
+        Width = (int)resolution;
+        Height = (int)resolution;
         NUM_FDTD = Width*Height;
         add =0;
-        EFields = new FDTD[NUM_FDTD];
-        HFields = new FDTD[NUM_FDTD];
         
         print(C);
         time = 0;
@@ -35,7 +35,21 @@ public class Driver : MonoBehaviour {
         TProp = Mh*NUM_FDTD*timeStep/C;
         dist = 1f;
 //        GameObject.Find("Distance").GetComponent<Text>().text = "" + NUM_FDTD*dist;
-
+        setupCells();
+        if(timeStep<(1/C*Mathf.Sqrt(1/dist))){
+            print("T is good");
+        }
+        else{
+            print("T too high. Should be < "+ (1/C*Mathf.Sqrt(1/dist)));
+        }
+        print("TPROP: " + TProp);
+        print("DistStep: " + dist);
+        print("TimeStep: " + timeStep);
+        setupGrid();
+    }
+    public void setupCells(){
+        EFields = new FDTD[NUM_FDTD];
+        HFields = new FDTD[NUM_FDTD];
         for(int i=0; i< NUM_FDTD; i++){
             //Create 600 FDTD cells
             FDTD h = new FDTD();
@@ -43,34 +57,17 @@ public class Driver : MonoBehaviour {
             HFields[i] = h;
             EFields[i] = e;
 
-            HFields[i].Position = Vector3.zero;
+            HFields[i].Position = new Vector3(i%resolution,Mathf.FloorToInt(i/resolution),0);
             HFields[i].Color = Random.ColorHSV();
-            HFields[i].coef = 37f;
+            HFields[i].coef = 1;
             HFields[i].time = 0;
-            //Coef
-            // HFields[i].coef = Mh;
-            // EFields[i].coef = Me;
-            // HFields[i].coef = 1;
-            // EFields[i].coef = 1;
-        }
-        for(int i=300; i< NUM_FDTD; i++){
-            //Coef
-            //HFields[i].coef = .1f;
-            //EFields[i].coef = .5f;
-        }
-        if(timeStep<(1/C*Mathf.Sqrt(1/dist))){
-            print("T is good");
-        }
-        else{
-            print("T too high. Should be < "+ (1/C*Mathf.Sqrt(1/dist)));
-        }
 
-        print("TPROP: " + TProp);
-        print("DistStep: " + dist);
-        print("TimeStep: " + timeStep);
-        setupGrid();
+            EFields[i].Position = new Vector3((i%resolution) + .5f, .5f + Mathf.FloorToInt(i/resolution),0);
+            EFields[i].Color = Random.ColorHSV();
+            EFields[i].coef = 1;
+            EFields[i].time = 0;
+        }
     }
-
     public void setupGrid() {
 
         GameObject grid = GameObject.Find("GridCanvas");
@@ -80,10 +77,14 @@ public class Driver : MonoBehaviour {
         render.filterMode = FilterMode.Point;
         render.Create();
         GameObject.Find("SimWindow").GetComponent<RawImage>().texture = render;
+        changeColor();
     }
 
     void Update(){
         debug();
+        if(HFields==null || EFields==null){
+            setupCells();
+        }
     }
     void FixedUpdate(){
         add/=1.5f;
@@ -114,18 +115,32 @@ public class Driver : MonoBehaviour {
     }
 
 
-    // void updateHField(){
-    //     for(int i=0; i<NUM_FDTD-1;i++){
-    //         // Update Equation for Every HField
-    //         HFields[i].Position.x = HFields[i].Position.x + (HFields[i].coef * ((EFields[i+1].Position.y - EFields[i].Position.y))/dist);
+    void updateHField(){
+        ComputeBuffer HFieldBuffer = new ComputeBuffer(HFields.Length,sizeof(float)*9);
+        ComputeBuffer EFieldBuffer = new ComputeBuffer(EFields.Length, sizeof(float)*9);
+        HFieldBuffer.SetData(HFields);
+        EFieldBuffer.SetData(EFields);
+        CellsCompute.SetBuffer(0,"HFields",HFieldBuffer);
+        CellsCompute.SetBuffer(0,"EFields", EFieldBuffer);
+        CellsCompute.SetFloat("dist", dist);
+        CellsCompute.Dispatch(0,HFields.Length,1,1);
 
-    //         Vector3 pos = Hline.GetPosition(i);
-    //         pos.y = HFields[i].Position.x;
-    //         Hline.SetPosition(i,pos);
-    //     }
-    //     HFields[NUM_FDTD-1].Position.x += (HFields[NUM_FDTD-1].coef * (E3 - EFields[NUM_FDTD-1].Position.y));
-    //     H3=H2; H2=H1; H1=HFields[0].Position.x;
+        HFieldBuffer.GetData(HFields);
 
+        HFieldBuffer.Release();
+        EFieldBuffer.Release();
+
+        // for(int i=0; i<NUM_FDTD-1;i++){
+        //     // Update Equation for Every HField
+        //     HFields[i].Position.x = HFields[i].Position.x + (HFields[i].coef * ((EFields[i+1].Position.y - EFields[i].Position.y))/dist);
+
+        //     Vector3 pos = Hline.GetPosition(i);
+        //     pos.y = HFields[i].Position.x;
+        //     Hline.SetPosition(i,pos);
+        // }
+        // HFields[NUM_FDTD-1].Position.x += (HFields[NUM_FDTD-1].coef * (E3 - EFields[NUM_FDTD-1].Position.y));
+        // H3=H2; H2=H1; H1=HFields[0].Position.x;
+    }
     // }
     // void updateEField(){
     //     EFields[0].Position.y += EFields[0].coef * (HFields[0].Position.y - E3);
@@ -166,7 +181,7 @@ public class Driver : MonoBehaviour {
         if(Input.GetKey(KeyCode.Q)){
             changeColor();
         }
-        if(Input.GetKeyDown(KeyCode.W)){
+        if(Input.GetKey(KeyCode.W)){
             for(int i=0; i<HFields.Length;i++){
                 if(HFields[i].Color != Color.green){ 
                     HFields[i].Color = Color.green;
@@ -174,15 +189,19 @@ public class Driver : MonoBehaviour {
                 }
             }
         }
-
-        if(Input.GetKeyDown(KeyCode.P)){
-            print("Num FDTD: " + NUM_FDTD);
-            print("Width: "+Width + "   Height: " + Height);
-        }
         if(Input.GetKeyDown(KeyCode.B)){
             for(int i =0; i<HFields.Length;i++){
                 HFields[i].Color = Color.black;
             }
+        }
+        if(Input.GetKeyDown(KeyCode.M)){
+            for(int i =0; i<HFields.Length;i++){
+                HFields[200].Position = new Vector3(255,255,255);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.E)){
+            updateHField();
         }
     }
 
@@ -190,12 +209,21 @@ public class Driver : MonoBehaviour {
         Vector2 pos;
         RectTransform r = GameObject.Find("SimWindow").GetComponent<RectTransform>();
         pos = Input.mousePosition;
-        pos.x = Mathf.FloorToInt(pos.x*2/resolution);
-        pos.y = Mathf.FloorToInt(pos.y*2/resolution);
-        print(pos);
-        print(Mathf.FloorToInt(pos.x)+Mathf.FloorToInt(pos.y)*resolution);
-        print(HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Color);
+        pos.x = Mathf.FloorToInt(resolution*(pos.x/r.rect.width));
+        pos.y = Mathf.FloorToInt(resolution*(pos.y/r.rect.height));
         HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Color = Color.red;
     }
 
+    public void onHover(){
+        Vector2 pos;
+        RectTransform r = GameObject.Find("SimWindow").GetComponent<RectTransform>();
+        pos = Input.mousePosition;
+        pos.x = Mathf.FloorToInt(resolution*(pos.x/r.rect.width));
+        pos.y = Mathf.FloorToInt(resolution*(pos.y/r.rect.height));
+        GameObject.Find("InfoText").GetComponent<Text>().text = "Info:\nCoords: "+ pos +
+            "\nColor: " + HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Color+
+            "\nPosition: " + HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].Position+
+            "\nCoef: " + HFields[(int)(pos.x)+(int)(Mathf.FloorToInt(pos.y)*resolution)].coef;
+
+    }
 }
